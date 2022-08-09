9e210f5003eab19ce3c24f28aa5b3b428cbda7fe
Absnormal <absnormal26065873@gmail.com>
2022-08-09 09:58:07 +0800

    [fix] route working on local

--- src/pages/DmgCalc.js
+++ src/pages/DmgCalc.js
@@ -4,7 +4,6 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import ResponsiveAppBar from '../components/ResponsiveAppBar';
 import SimpleCalc from '../components/SimpleCalc';
 
 function DmgCalc() {
@@ -16,7 +15,6 @@ function DmgCalc() {
 
 	return (
 		<div className="DmgCalc">
-			<ResponsiveAppBar />
 			<Box sx={{ width: '100%', typography: 'body1' }}>
 				<TabContext value={value}>
 					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
